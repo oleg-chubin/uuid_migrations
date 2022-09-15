c3a9954bc2fc9ff1f70843cf109d38e7939f347b
o_chubin <oleg.chubin@gmail.com>
2022-09-15 15:07:59 +0300

    change pk field

--- core_base/models.py
+++ core_base/models.py
@@ -5,10 +5,9 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=120)
-    uuid_hash = models.UUIDField(default=uuid.uuid4, unique=True)
+    uuid_hash = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
 
 
 class Participant(models.Model):
     name = models.CharField(max_length=120)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
     team_uuid = models.UUIDField()
